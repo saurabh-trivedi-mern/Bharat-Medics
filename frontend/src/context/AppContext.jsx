59e0ccef6db70c7d0e5e3f3ce809b3b2b82ac36c
Saurabh Trivedi <145359687+saurabh-trivedi-mern@users.noreply.github.com>
2025-01-11 19:37:49 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = https://hindustan-medics.onrender.com;
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
